536c4acd940512a677d7ba3a6d493f7b3bf4ef95
lutfi <83458160+lutfi-sys@users.noreply.github.com>
2024-10-07 20:35:41 +0700

    Your commit message

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -42,7 +42,7 @@
 <body>
     <header id="header">
         <nav
-            class=" z-10 fixed block w-full max-w-screen-2xl py-4 px-8 shadow-md w-full max-w-full rounded-none px-4 shadow-none bg-gradient-to-r from-green-800 via-green-800 to-teal-0">
+            class=" z-10 fixed block w-full max-w-screen-2xl py-4 px-8    rounded-none  shadow-none bg-gradient-to-r from-green-800 via-green-800 to-teal-0">
             <div class="container mx-auto flex items-center justify-between text-blue-gray-900">
                 <a href="#"
                     class="block antialiased font-sans  text-base  leading-relaxed text-yellow-500 mr-4 cursor-pointer  font-bold ">Yayasan
@@ -51,46 +51,52 @@
                     <ul class="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
                         <a href="/">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Home
                             </li>
                         </a><a href="/about">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Tentang Kami
                             </li>
                         </a><a href="/info">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Berita
                             </li>
                         </a><a href="/perpus">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Perpustakaan
                             </li>
                         </a><a href="/alumni">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Pojok Alumni
                             </li>
                         </a><a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 PPDB
                             </li>
                         </a><a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed hover:text-green-200 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed hover:text-green-200 p-1 font-medium">
                                 Hubungi Kami
                             </li>
                         </a>
                     </ul>
                 </div>
-                <button
-                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none bg-gray-900 hidden lg:inline-block"
+                {{-- <button href="/login" class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none  hidden lg:inline-block"
                     type="button" data-ripple-light="true">
-                    Sign in</button><button
+                    Sign in</button> --}}
+                    <a href="/login">
+                        <li
+                        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none hidden lg:inline-block">
+                            Sign in
+                        </li>
+                    </a>
+                    <button
                     class="relative align-middle select-none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-8 max-w-[32px] h-8 max-h-[32px] rounded-lg text-xs text-blue-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30 ml-auto inline-block text-blue-gray-900 lg:hidden"
                     type="button" data-ripple-dark="true" data-collapse-target="navbar">
                     <span class="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2"><svg
@@ -107,40 +113,40 @@
                     <ul class="mb-4 mt-2 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-8">
                         <a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 Tentang Kami
                             </li>
                         </a><a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 Berita
                             </li>
                         </a><a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 Perpustakaan
                             </li>
                         </a>
                         <a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 Pojok Alumni
                             </li>
                         </a>
                         <a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base  leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 PPDB
                             </li>
                         </a><a href="#">
                             <li
-                                class="block antialiased font-sans text-base font-light leading-relaxed text-blue-gray-900 p-1 font-medium">
+                                class="block antialiased font-sans text-base leading-relaxed text-blue-gray-900 p-1 font-medium">
                                 Hubungi Kami
                             </li>
                         </a>
                     </ul>
                     <button
-                        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none bg-gray-900 block w-full mb-2"
+                        class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg  text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none bg-gray-900 block w-full mb-2"
                         type="button" data-ripple-light="true">
                         Sign in
                     </button>
